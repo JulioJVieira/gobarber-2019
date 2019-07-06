4ccbed851be7c8ea40d36f9597728249272ec57d
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-06 00:12:39 -0300

    password hash

--- module02/src/app/models/User.js
+++ module02/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,7 +15,10 @@ class User extends Model {
         sequelize,
       }
     );
-    return this;
+
+    this.addHook('beforeSave', async user => {
+      user.password_hash = await bcrypt.hash(user.password, 8);
+    });
   }
 }
 
