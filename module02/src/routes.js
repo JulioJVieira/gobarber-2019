750d16b923d418a78fb83b4b190f2b66c981fb16
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-05 23:43:32 -0300

    models loader

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Julio Vieira',
+    email: 'julio@rocketseat.com.br',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
