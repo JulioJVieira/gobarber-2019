a5732803169e91ad3048ae4a5d46c43f19eba3ad
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-10 19:54:38 -0300

    Create Appointment

--- module03/src/app.js
+++ module03/src/app.js
@@ -23,4 +23,5 @@ class App {
     this.server.use(routes);
   }
 }
+
 export default new App().server;
--- module03/src/app/models/Appointment.js
+++ module03/src/app/models/Appointment.js
@@ -1,6 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
-class File extends Model {
+class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
@@ -15,9 +15,9 @@ class File extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.user, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.user, { foreignKey: 'provider_id', as: 'provider' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 
-export default File;
+export default Appointment;
