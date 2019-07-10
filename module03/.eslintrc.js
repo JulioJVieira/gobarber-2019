87d21aba891dfc29149a9a812d5743be54af6bf7
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-10 20:25:33 -0300

    eslint camelcase config off

--- module03/.eslintrc.js
+++ module03/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
-    "camlcase": "off",
+    "camelcase": "off",
     "no-unused-vars": ["error", { "argsIgnorePattern": "next"}],
   },
 };
