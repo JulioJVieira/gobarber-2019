777a8541c92690ae3b808e7cdc855482b4af1bdc
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-12 19:45:58 -0300

    Nodemailer config

---
+++ module03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '73956b37e270ee',
+    pass: '11a280a4748f6c',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com',
+  },
+};
