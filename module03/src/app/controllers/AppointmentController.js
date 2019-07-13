c84e2d85272817303a0d8bc6c10b63badac8f340
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-12 21:54:28 -0300

    Redis Queue Config

--- module03/src/app/controllers/AppointmentController.js
+++ module03/src/app/controllers/AppointmentController.js
@@ -2,7 +2,8 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/cancellationMail';
+import Queue from '../../lib/Queue';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
@@ -171,17 +172,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
