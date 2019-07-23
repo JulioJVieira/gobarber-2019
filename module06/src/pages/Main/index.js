02998d33784fd819e9a964ed701852c926d2e86e
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-23 00:00:34 -0300

    Form Style

--- module06/src/pages/Main/index.js
+++ module06/src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCaptalize="none"
+          placeholder="Adicionar usuário"
+        />
+
+        <SubmitButton>
+          <Icon name="add" color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
