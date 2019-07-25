c5b6c8f4a74b435650a79e796de47dc2d1c8b6a5
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-24 21:20:39 -0300

    Styles - Home/Cart

--- module07/src/App.js
+++ module07/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 import Routes from './routes';
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      {/* <Header /> */}
+      <Header />
       <Routes />
     </BrowserRouter>
   );
