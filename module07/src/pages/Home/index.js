dafa097c64aaa8b0921e01877be8d6de1a9b0a19
Julio Vieira <julio_vieirak8@hotmail.com>
2019-07-29 19:48:32 -0300

    Redux Saga Config

--- module07/src/pages/Home/index.js
+++ module07/src/pages/Home/index.js
@@ -26,10 +26,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -44,7 +44,10 @@ class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button" onClick={() => this.handleProduct(product)}>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product.id)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />{' '}
                 {amount[product.id] || 0}
