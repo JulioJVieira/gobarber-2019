d1736357506b06cda2ab0695d3f94709b8387f5f
Julio Vieira <julio_vieirak8@hotmail.com>
2019-08-06 00:43:05 -0300

    components to hooks

--- module07/.eslintrc.js
+++ module07/.eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -32,6 +33,7 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
-    'no-console': ["error", {allow: ["tron"]}]
+    'no-console': ["error", {allow: ["tron"]}],
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
